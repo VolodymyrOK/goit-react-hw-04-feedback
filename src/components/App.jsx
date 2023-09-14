6fd1dbc43bb5738f778c8047586161482967c6e4
Volodymyr <vladikon70@gmail.com>
2023-09-14 17:09:41 +0200

    update files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,20 @@ import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
-import { init, reducer } from './Reducer/Reducer';
+// import { init, reducer_3 } from './Reducer/Reducer';
+// import { reducer_2 } from './Reducer/Reducer';
+import { reducer_1 } from './Reducer/Reducer';
 
 export const App = () => {
   const [review, dispatch] = useReducer(
-    reducer,
-    { good: 0, neutral: 0, bad: 0 },
-    init
+    reducer_1,
+    { good: 0, neutral: 0, bad: 0 }
+    // init //function reducer_3
   );
 
   const onReload = () => {
-    dispatch({ type: 'reset', increase: { good: 0, neutral: 0, bad: 0 } });
+    // dispatch({ type: 'reset', increase: { good: 0, neutral: 0, bad: 0 } }); //function reducer_3
+    dispatch({ type: 'reset' }); //function reducer_2 and reducer_1
   };
 
   const { good, neutral, bad } = review;
@@ -26,11 +29,23 @@ export const App = () => {
     (good / (total || 1)) * 100
   );
 
+  // function reducer_1
   const onLeaveFeedback = key => {
     dispatch({
-      type: key,
-      increase: 1,
+      type: 'increment',
+      payload: key,
     });
+
+    // function reducer_2
+    // dispatch({
+    //   type: key,
+    // });
+
+    // function reducer_3
+    // dispatch({
+    //   type: key,
+    //   increase: 1,
+    // });
   };
 
   const options = Object.keys(review);
--- src/components/Reducer/Reducer.js
+++ src/components/Reducer/Reducer.js
@@ -1,4 +1,46 @@
-export const reducer = (state, action) => {
+export const reducer_1 = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return {
+        ...state,
+        [action.payload]: state[action.payload] + 1,
+      };
+    case 'reset':
+      return { good: 0, neutral: 0, bad: 0 };
+
+    default:
+      return state;
+  }
+};
+
+// function reducer_2
+export const reducer_2 = (state, action) => {
+  switch (action.type) {
+    case 'good':
+      return {
+        ...state,
+        good: state.good + 1,
+      };
+    case 'neutral':
+      return {
+        ...state,
+        neutral: state.neutral + 1,
+      };
+    case 'bad':
+      return {
+        ...state,
+        bad: state.bad + 1,
+      };
+    case 'reset':
+      return { good: 0, neutral: 0, bad: 0 };
+
+    default:
+      return state;
+  }
+};
+
+// function reducer_3
+export const reducer_3 = (state, action) => {
   switch (action.type) {
     case 'good':
       return {
@@ -17,7 +59,6 @@ export const reducer = (state, action) => {
       };
     case 'reset':
       return init(action.increase);
-
     default:
       return state;
   }
